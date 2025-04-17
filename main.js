3f0ae31ac3a53c95e5ead0a8ef725ae5d029c92e
AlbertoPimentel <albertopimentel04@gmail.com>
2025-04-17 13:42:45 -0600

    version 2

--- main.js
+++ main.js
@@ -119,18 +119,3 @@ document.getElementById('btnInvitacion').addEventListener('click', () => {
   function closeRSVP() {
     document.getElementById("rsvp-modal").style.display = "none";
   }
-
-
-  let currentIndex = 0;
-
-  function moveCarousel(direction) {
-    const track = document.getElementById("carousel-track");
-    const slides = document.querySelectorAll(".carousel-slide");
-    currentIndex += direction;
-
-    if (currentIndex < 0) currentIndex = slides.length - 1;
-    if (currentIndex >= slides.length) currentIndex = 0;
-
-    const offset = -currentIndex * 100;
-    track.style.transform = `translateX(${offset}%)`;
-  }
